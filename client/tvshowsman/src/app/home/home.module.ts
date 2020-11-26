e20a2aefe1bebbd608088779dc123dd6fb93ad67
med-alaa-rb <alaa.rabai@gmail.com>
2020-11-26 17:53:30 +0100

    err button solved

--- client/tvshowsman/src/app/home/home.module.ts
+++ client/tvshowsman/src/app/home/home.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
 import { FormsModule } from "@angular/forms";
 import { HomePage } from "./home.page";
-import { HttpService } from "../http.service";
-import { LocalService } from "../local.service";
-import { Router } from "@angular/router";
 
 import { HomePageRoutingModule } from "./home-routing.module";
 
@@ -14,13 +11,5 @@ import { HomePageRoutingModule } from "./home-routing.module";
   declarations: [HomePage],
 })
 export class HomePageModule {
-  constructor(
-    private _http: HttpService,
-    private local: LocalService,
-    private router: Router
-  ) {}
-  nextStep() {
-    // this.router.navigateByUrl("list-shows");
-    // console.log("hello");
-  }
+  constructor() {}
 }
--- client/tvshowsman/src/app/home/home.page.ts
+++ client/tvshowsman/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpService } from "../http.service";
+import { LocalService } from "../local.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(
+    private _http: HttpService,
+    private local: LocalService,
+    private router: Router
+  ) {}
 
-  constructor() {}
-
+  async advance() {
+    this.router.navigateByUrl("list-shows");
+    await console.log("hello");
+  }
 }
